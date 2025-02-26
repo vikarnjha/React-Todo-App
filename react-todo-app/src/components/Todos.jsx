7f983befd431cbed936865e86240b00c994e9ba9
Vikarn Jha <vikarnjha91@gmail.com>
2025-02-26 14:04:18 +0530

    completed till input text abd add it to an array

--- react-todo-app/src/components/Todos.jsx
+++ react-todo-app/src/components/Todos.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Todos = () => {
+  const [input, setInput] = useState('')
+  const [todos, setTodos] = useState([])
+  const getInput = (e) => {
+    setInput(e.target.value)
+    // console.log("Inputed text :- ", setInput)
+  }
+  const getInputData = () => {
+    // console.log(input)
+    let store = [...todos, input]
+    setTodos(store)
+    // setTodos(...todos, input)
+    setInput('')
+    // console.log(todos)
+  }
   return (
     <div className='container'>
         <div className='inputTask'>
-          <input type='text' placeholder='Add todo...' />
-          <button>Add</button>
+          <input type='text' placeholder='Add todo...' onChange={getInput} />
+          <button onClick={getInputData}>Add</button>
         </div>
+        {todos}
     </div>
   )
 }
