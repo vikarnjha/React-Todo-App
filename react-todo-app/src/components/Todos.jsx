bfde13ea5fde4bc3e45e248f0ba6ae6157df0d3b
Vikarn Jha <vikarnjha91@gmail.com>
2025-02-26 14:30:30 +0530

    added delete icon and structured the todos display

--- react-todo-app/src/components/Todos.jsx
+++ react-todo-app/src/components/Todos.jsx
@@ -1,29 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 const Todos = () => {
-  const [input, setInput] = useState('')
-  const [todos, setTodos] = useState([])
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
   const getInput = (e) => {
-    setInput(e.target.value)
+    setInput(e.target.value);
     // console.log("Inputed text :- ", setInput)
-  }
+  };
   const getInputData = () => {
     // console.log(input)
-    let store = [...todos, input]
-    setTodos(store)
+    let store = [...todos, input];
+    setTodos(store);
     // setTodos(...todos, input)
-    setInput('')
+    setInput("");
     // console.log(todos)
-  }
+  };
   return (
-    <div className='container'>
-        <div className='inputTask'>
-          <input type='text' placeholder='Add todo...' onChange={getInput} />
-          <button onClick={getInputData}>Add</button>
+    <div className="container">
+      <div className="inputTask">
+        <input
+          type="text"
+          placeholder="Add todo..."
+          value={input}
+          onChange={getInput}
+        />
+        <button onClick={getInputData}>Add</button>
+      </div>
+      {todos.map((todo, index) => (
+        <div key={index} className="todo">
+          {todo} <FontAwesomeIcon icon={faTrash} />
         </div>
-        {todos}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Todos
+export default Todos;
